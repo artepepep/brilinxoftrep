2d32b8215d3428c54758d1d44cf03af2be3bbfe3
artempepep <artem1884@outlook.com>
2024-04-02 21:27:57 +0300

    add vercel cors allowed headers

--- brilinxsoft/brilinxsoft/settings.py
+++ brilinxsoft/brilinxsoft/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
 # CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-"https://www.brilinxoft.com",
+    "https://www.brilinxoft.com",
+    'https://brilinext.vercel.app/',
 ]
 
 MIDDLEWARE = [
