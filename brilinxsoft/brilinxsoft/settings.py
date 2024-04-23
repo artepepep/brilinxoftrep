f25efd166a1e6f4f93a35d5242ca2ad287dc3ebd
mac <Artem1884@outlook.com>
2024-04-23 22:01:05 +0200

    makemigrations and other things

--- brilinxsoft/brilinxsoft/settings.py
+++ brilinxsoft/brilinxsoft/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://www.brilinxoft.com",
-    'https://brilinext.vercel.app/',
+    'https://brilinext.vercel.app',
 ]
 
 MIDDLEWARE = [
@@ -77,14 +77,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'brilinxsoft.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'PORT': os.environ.get('DB_PORT'),
+#         'HOST': os.environ.get('DB_HOST')
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'PORT': os.environ.get('DB_PORT'),
-        'HOST': os.environ.get('DB_HOST')
+        'NAME': 'postgres',
+        'USER': 'postgres.ktthzpvnuzgbognvccuv',
+        'PASSWORD': 'Artem22062006!!',
+        'PORT': '5432',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com'
     }
 }
 
--- brilinxsoft/posts/serializers.py
+++ brilinxsoft/posts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Post
 
-class PostSerializer(serializers.Modelserializer):
+class PostSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Post
